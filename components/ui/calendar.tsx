4ea50d5e9320de4bceb7f02a51a0587a4084f04a
Markus <markus.moetz@me.com>
2025-04-16 04:12:19 +0200

    fix to deploy #2

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -54,8 +54,16 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />
+        PreviousMonthButton: (props) => (
+          <button {...props} className={props.className}>
+            <ChevronLeft className="h-4 w-4" />
+          </button>
+        ),
+        NextMonthButton: (props) => (
+          <button {...props} className={props.className}>
+            <ChevronRight className="h-4 w-4" />
+          </button>
+        )
       }}
       {...props}
     />
